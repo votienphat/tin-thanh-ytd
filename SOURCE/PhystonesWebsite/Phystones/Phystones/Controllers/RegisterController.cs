a82f107168fc31d69808a8c8e6ffdf7dabc5b8f3
Phat Vo <votienphat@gmail.com>
2017-07-09 23:12:32 +0700

    [Phystones] Lấy dữ liệu từ site khác về

--- SOURCE/PhystonesWebsite/Phystones/Phystones/Controllers/RegisterController.cs
+++ SOURCE/PhystonesWebsite/Phystones/Phystones/Controllers/RegisterController.cs
@@ -33,8 +33,10 @@ namespace Phystones.Controllers
         // GET: Sample
         public ActionResult Index()
         {
+            
             return View();
         }
+
         [HttpPost]
         public ActionResult SendMess(SendContact model)
         {
@@ -47,16 +49,19 @@ namespace Phystones.Controllers
             }
             return PartialView();
         }
+
         public ActionResult SendMess()
         {
             var model = new Register();
             return PartialView(model);
         }
+
         public ActionResult SaveData()
         {
             var model = new SimpleDataModel();
             return PartialView(model);
         }
+
         [HttpPost]
         public ActionResult SaveData(SimpleDataModel model)
         {
@@ -66,6 +71,7 @@ namespace Phystones.Controllers
             }
             return PartialView();
         }
+
         public JsonResult DataCompany(string Name, int Type)
         {
             string urlAddress = "http://www.hosocongty.vn/search.php?key=" + Name + "&ot=" + Type + "&p=0&d=0";
@@ -75,5 +81,97 @@ namespace Phystones.Controllers
 
             return new JsonResult() { Data = rootDocument, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
+
+        private HoSoCongTyItem GetCompanyInfo(string mst)
+        {
+            string urlAddress = "http://www.hosocongty.vn/json/home.php?page=1&last_id=0&province_id=0&district_id=0&q=" + mst + "&ot=0";
+            var html = new HtmlDocument();
+            html.LoadHtml(new WebClient().DownloadString(urlAddress));
+            var jsonContent = html.DocumentNode.InnerHtml;
+            HoSoCongTySearch json = JsonConvert.DeserializeObject<HoSoCongTySearch>(jsonContent);
+            HoSoCongTyItem result = new HoSoCongTyItem();
+            var item = json.Items.FirstOrDefault();
+
+            if (item != null)
+            {
+                urlAddress = "http://www.hosocongty.vn/" + item.Link.Replace("./", "");
+                html.LoadHtml(new WebClient().DownloadString(urlAddress));
+                byte[] bytes = Encoding.Default.GetBytes(html.DocumentNode.InnerHtml);
+                var htmlContent = Encoding.UTF8.GetString(bytes);
+                html.LoadHtml(htmlContent);
+                var tag = html.DocumentNode
+                    .Descendants("div")
+                    .FirstOrDefault(x => x.Attributes["class"].Value == "companyDetail");
+                if (tag != null)
+                {
+                    var companyNameTag = tag
+                        .Descendants("h1")
+                        .FirstOrDefault();
+                    result.CompanyName = companyNameTag == null ? "" : companyNameTag.InnerText;
+
+                    companyNameTag = tag
+                        .Descendants("p")
+                        .FirstOrDefault(x => x.InnerText.Contains("Địa chỉ"));
+                    if (companyNameTag != null)
+                    {
+                        var addressTag = companyNameTag
+                            .Descendants("strong")
+                            .FirstOrDefault();
+                        result.Address = addressTag == null ? "" : addressTag.InnerText;
+                    }
+
+                    companyNameTag = tag
+                        .Descendants("p")
+                        .FirstOrDefault(x => x.InnerText.Contains("Mã số thuế"));
+                    if (companyNameTag != null)
+                    {
+                        var addressTag = companyNameTag
+                            .Descendants("strong")
+                            .FirstOrDefault();
+                        result.MST = addressTag == null ? "" : addressTag.InnerText;
+                    }
+
+                    companyNameTag = tag
+                        .Descendants("li")
+                        .FirstOrDefault(x => x.InnerText.Contains("Giám đốc"));
+                    if (companyNameTag != null)
+                    {
+                        var addressTag = companyNameTag
+                            .Descendants("strong")
+                            .LastOrDefault();
+                        result.CEO = addressTag == null ? "" : addressTag.InnerText;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class HoSoCongTyItem
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string CompanyName { get; set; }
+
+        [JsonProperty("url")]
+        public string Link { get; set; }
+
+        [JsonProperty("mst")]
+        public string MST { get; set; }
+
+        [JsonProperty("add")]
+        public string Address { get; set; }
+
+        [JsonProperty("CEO")]
+        public string CEO { get; set; }
+    }
+
+    public class HoSoCongTySearch
+    {
+        [JsonProperty("album")]
+        public List<HoSoCongTyItem> Items { get; set; }
     }
 }
