142aeabac1546c295dcfe1e15357e627e7634689
Phat Vo <votienphat@gmail.com>
2017-07-11 22:09:15 +0700

    [Phystones] Thêm route multi language

--- Source/PhystonesWebsite/Phystones/Phystones/Helper/GlobalHelper.cs
+++ Source/PhystonesWebsite/Phystones/Phystones/Helper/GlobalHelper.cs
@@ -1,11 +1,16 @@
 ﻿using System.Configuration;
 using System.Threading;
 using System.Web.Configuration;
+using MyUtility.Extensions;
+using Phystones.Models.Enum;
 
 namespace Phystones.Helper
 {
     public class GlobalHelper
     {
+        public const string Vietnamese = "vi-VN";
+        public const string English = "en-US";
+
         public static string CurrentCulture
         {
             get
@@ -21,6 +26,21 @@ namespace Phystones.Helper
                 GlobalizationSection section = (GlobalizationSection)config.GetSection("system.web/globalization");
                 return section.UICulture;
             }
-        }     
+        }
+
+        public static string GetRouteName(RouteName routeName)
+        {
+            return CurrentCulture + routeName.Text();
+        }
+
+        public static string GetEnglishRouteName(RouteName routeName)
+        {
+            return English + routeName.Text();
+        }
+
+        public static string GetVietnameseRouteName(RouteName routeName)
+        {
+            return Vietnamese + routeName.Text();
+        }
     }
 }
